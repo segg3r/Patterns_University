31f36c0082a54f27e5c9aa1724976ad9a15d7cee
segg3r <segg3r@gmail.com>
2014-05-02 14:24:54 +0300

    Task 3 comments

--- src/Task3InputRunner.java
+++ src/Task3InputRunner.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -13,20 +12,21 @@ public class Task3InputRunner {
 	public static void main(String[] args) {
 		int c;
 		try {
+			// Multiple space character by character
 			Reader in = new MultipleSpaceReader(new LowerCaseReader(
 					new BufferedReader(new FileReader("test.txt"))));
 			while ((c = in.read()) >= 0) {
 				System.out.print((char) c);
 			}
 			System.out.println();
-			//
+			// Upper case
 			in = new UpperCaseReader(new BufferedReader(new FileReader(
 					"test.txt")));
 			while ((c = in.read()) >= 0) {
 				System.out.print((char) c);
 			}
 			System.out.println();
-			//
+			// Multiple space all characters
 			in = new MultipleSpaceReader(new LowerCaseReader(
 					new BufferedReader(new FileReader("test.txt"))));
 			int maxLength = 1000;
